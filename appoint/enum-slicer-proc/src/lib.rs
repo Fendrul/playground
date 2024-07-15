64fc9c36498db38ab3f31a47df806e1a698c2732
Simon <simon.strykers@gmail.com>
2024-07-15 15:24:20 +0200

    docs: added documentation for the EnumSlice macro

--- appoint/enum-slicer-proc/src/lib.rs
+++ appoint/enum-slicer-proc/src/lib.rs
@@ -3,6 +3,35 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{Data, DeriveInput, Fields, parse_macro_input};
 
+/// This macro derives an implementation of the `IntoEnumSlice` trait for enums with unit variants.
+///
+/// The `EnumSlice` derive macro can only be applied to enums. It generates an implementation
+/// of the `IntoEnumSlice` trait, which provides a method to get a slice of all the enum variants.
+///
+/// # Example
+///
+/// ```
+/// use enum_slicer_proc::EnumSlice;
+/// use enum_slicer::IntoEnumSlice; 
+/// 
+/// #[derive(EnumSlice, Debug, PartialEq)] 
+/// enum MyEnum { 
+///     Variant1, 
+///     Variant2, 
+///     Variant3, 
+/// } 
+/// 
+/// fn main() { 
+///    let variants = MyEnum::variants_slice(); 
+///    assert_eq!(variants, &[MyEnum::Variant1, MyEnum::Variant2, MyEnum::Variant3]); 
+/// } 
+/// ```
+///
+/// # Panics
+///
+/// This macro will panic if:
+/// - It is applied to a non-enum type.
+/// - The enum contains non-unit variants (i.e., variants with fields).
 #[proc_macro_derive(EnumSlice)]
 pub fn derive_enum_iterator(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
--- appoint/enum-slicer/src/lib.rs
+++ appoint/enum-slicer/src/lib.rs
@@ -1,3 +1,5 @@
 pub trait IntoEnumSlice {
-    fn variants_slice<'a>() -> &'a [Self] where Self: Sized;
+    fn variants_slice<'a>() -> &'a [Self]
+    where
+        Self: Sized;
 }
